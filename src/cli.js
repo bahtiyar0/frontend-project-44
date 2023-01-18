a806a10d63ef42e383f9a6fac10cf2f6728c22c7
bahtiyar0 <121649250+bahtiyar0@users.noreply.github.com>
2023-01-19 03:08:34 +0500

    added

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,4 @@
-#!/usr/bin/env node
+var readlineSync = require('readline-sync');
+const name = readlineSync.question('Your answer: May I have your name?');
+console.log('Hello ' + userName + '!');
+export default;
