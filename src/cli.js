6377b325e2aa54cf9413b993f2bd161114991369
bahtiyar0 <121649250+bahtiyar0@users.noreply.github.com>
2023-01-19 03:37:39 +0500

    added

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-
 const greeting = () => {
     const userName = readlineSync.question('May I have your name?');
     console.log('Welcome to the Brain Games!');
