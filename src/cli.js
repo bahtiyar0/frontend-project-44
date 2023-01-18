17cd6e715c8fe81e8deda6565b28898d34da2fa7
bahtiyar0 <121649250+bahtiyar0@users.noreply.github.com>
2023-01-19 03:24:47 +0500

    added

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,8 @@
-var readlineSync = require('readline-sync');
-const name = readlineSync.question('Your answer: May I have your name?');
-console.log('Hello ' + userName + '!');
-export default;
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+    const userName = readlineSync.question('May I have your name?');
+    console.log('Welcome to the Brain Games!');
+    console.log(`Hello, ${userName}!`);
+};
+export default greeting;
