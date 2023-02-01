97d1be83ee468470263f775f21c5231157163e39
bahtiyar0 <121649250+bahtiyar0@users.noreply.github.com>
2023-02-02 02:21:29 +0500

    added all

---
+++ src/cmz.js
@@ -0,0 +1,45 @@
+import readlineSync from 'readline-sync';
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const operatorArray = ['+', '-', '*'];
+const calculate = (number1, number2, operator) => {
+    switch (operator) {
+        case '+':
+            return number1 + number2;
+        case '-':
+            return number1 - number2;
+        case '*':
+            return number1 * number2;
+        default:
+            throw new Error(`Unkown order state: '${operator}'!`);
+    }
+};
+
+const startGame = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+    console.log('What is the result of the expression?')
+    let sum = 0;
+    for (let i = 0; i < 3; i += 1) {
+        const number1 = getRandomNumber(1, 100);
+        const number2 = getRandomNumber(1, 100);
+        const index = getRandomNumber(0, operatorArray.length - 1);
+        const selectedOperator = operatorArray[index];
+        const operator = selectedOperator;
+        const question = `${number1} ${operator} ${number2}`;
+        console.log(`Question: ${question}`);
+        const answer = readlineSync.question('Your answer: ');
+        const result = calculate(number1, number2, operator);
+        if (answer === result) {
+            console.log('Correct!');
+            sum += 1;
+        } else {
+            console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.`);
+            console.log(`Let's try again, ${userName}!`);
+        }
+    }
+    if (sum === 3) {
+        console.log(`Congratulations, ${userName}!`);
+    }
+};
+
+export default startGame;
